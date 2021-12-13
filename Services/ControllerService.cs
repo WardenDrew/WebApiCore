8132f4f561e673e9cdf373e1040bde668fd907f4
Andrew Haskell <32055840+WardenDrew@users.noreply.github.com>
2021-12-12 18:11:57 -0800

    1.0.1 - fix controllerService.Request naming

--- Services/ControllerService.cs
+++ Services/ControllerService.cs
@@ -9,26 +9,26 @@ namespace WebApiCore.Services
 	public interface IControllerService : IServiceScanningServiceInterface
 	{
 		/// <summary>
-		/// Executes a MediatR request and returns the response wrapped in an IResult
-		/// </summary>
-		/// <remarks>
-		/// Use with AspNetCore Minimal API's. For AspNetCore MVC (Controllers) use <see cref="RequestMvc{TResult}(IRequest{TResult})">RequestMvc()</see> instead
-		/// </remarks>
-		/// <typeparam name="TResult"></typeparam>
-		/// <param name="request"></param>
-		/// <returns></returns>
-		public Task<IResult> Request<TResult>(IRequest<TResult> request) where TResult : IResponse;
-
-		/// <summary>
 		/// Executes a MediatR request and returns the response wrapped in an IActionResult
 		/// </summary>
 		/// <remarks>
-		/// Use with AspNetCore MVC (Controllers). For AspNetCore Minimal API's use <see cref="Request{TResult}(IRequest{TResult})">Request()</see> instead
+		/// Use with AspNetCore MVC (Controllers). For AspNetCore Minimal API's use <see cref="RequestMinimal{TResult}(IRequest{TResult})">RequestMinimal()</see> instead
 		/// </remarks>
 		/// <typeparam name="TResult"></typeparam>
 		/// <param name="request"></param>
 		/// <returns></returns>
-		public Task<IActionResult> RequestMvc<TResult>(IRequest<TResult> request) where TResult : IResponse;
+		public Task<IActionResult> Request<TResult>(IRequest<TResult> request) where TResult : IResponse;
+
+		/// <summary>
+		/// Executes a MediatR request and returns the response wrapped in an IResult
+		/// </summary>
+		/// <remarks>
+		/// Use with AspNetCore Minimal API's. For AspNetCore MVC (Controllers) use <see cref="Request{TResult}(IRequest{TResult})">Request()</see> instead
+		/// </remarks>
+		/// <typeparam name="TResult"></typeparam>
+		/// <param name="request"></param>
+		/// <returns></returns>
+		public Task<IResult> RequestMinimal<TResult>(IRequest<TResult> request) where TResult : IResponse;
 	}
 	
 	public class ControllerService : IControllerService, IServiceScanningScopedImplementation
@@ -40,18 +40,7 @@ namespace WebApiCore.Services
 			this.mediator = mediator;
 		}
 
-		public async Task<IResult> Request<TResult>(IRequest<TResult> request) where TResult : IResponse
-		{
-			IResponse response = await mediator.Send(request);
-
-			return Results.Extensions.ContentStatus(
-				content: response.Serialize(),
-				contentType: System.Net.Mime.MediaTypeNames.Application.Json,
-				contentEncoding: System.Text.Encoding.UTF8,
-				statusCode: response.CalculateStatusCode());
-		}
-
-		public async Task<IActionResult> RequestMvc<TResult>(IRequest<TResult> request) where TResult : IResponse
+		public async Task<IActionResult> Request<TResult>(IRequest<TResult> request) where TResult : IResponse
 		{
 			IResponse result = await mediator.Send(request);
 
@@ -61,6 +50,17 @@ namespace WebApiCore.Services
 			cResult.ContentType = System.Net.Mime.MediaTypeNames.Application.Json;
 
 			return cResult;
+		}
+
+		public async Task<IResult> RequestMinimal<TResult>(IRequest<TResult> request) where TResult : IResponse
+		{
+			IResponse response = await mediator.Send(request);
+
+			return Results.Extensions.ContentStatus(
+				content: response.Serialize(),
+				contentType: System.Net.Mime.MediaTypeNames.Application.Json,
+				contentEncoding: System.Text.Encoding.UTF8,
+				statusCode: response.CalculateStatusCode());
 		}
 	}
 }
